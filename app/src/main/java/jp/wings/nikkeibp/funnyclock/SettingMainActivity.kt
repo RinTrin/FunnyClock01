feefae1fbd91e29164d7d5450519d21f1a3577c1
RinTrin <tomirin.rinta@gmail.com>
2020-04-18 15:02:00 +0900

    Initial commit

---
+++ app/src/main/java/jp/wings/nikkeibp/funnyclock/SettingMainActivity.kt
@@ -0,0 +1,12 @@
+package jp.wings.nikkeibp.funnyclock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
